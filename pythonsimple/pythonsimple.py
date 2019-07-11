fc003c870008006d74d11efa76cc4c5281430f9d
Terry Kaleas <terry.kaleas@gmail.com>
2019-07-11 07:12:36 -0700

    add zen of python and vscode workspace

--- pythonsimple/pythonsimple.py
+++ pythonsimple/pythonsimple.py
@@ -6,13 +6,14 @@ import sys
 #internal imports
 import helpers
 
+#helpers is a template for internally imported modules
 from helpers import HelperSample
 
 def main():
 	print("Hello World!")
 	HelperSample.Helper1()
 	HelperSample.Helper2()
-	hSample = HelperSample();
+	hSample = HelperSample()
 	hSample.Helper3()
 	
 if __name__== "__main__":
