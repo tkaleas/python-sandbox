05c37cdf73f849d9c23966621a47ce2024a277de
Terry Kaleas <terry.kaleas@gmail.com>
2019-08-13 20:47:34 -0700

    update with remove chars

--- problems/remove_chars.py
+++ problems/remove_chars.py
@@ -8,10 +8,12 @@
 # construct a hashmap of the removeable characters -> bool map, remove or do not remove.
 # two pointers, one is parsing the strings, and one is removing the things in the hashmap
 
+#Runtime: O(n+m)
 
 def removeSpecifiedChars(inputString, removeString):
-
+ 
     removeCharList = [False]*256    # Assumes ASCII string, for unicode would be 0 to 65535
+    outputString = bytearray(inputString,'utf-8')
 
     #Parse Remove String
     for c in removeString:
@@ -22,5 +24,13 @@ def removeSpecifiedChars(inputString, removeString):
     insertPoint = 0
     for i in range(0,len(inputString)):
         curChar = inputString[i]
-        # if its in the char list
-        if removeCharList[ord(curChar)]:
+        # if its not in the char list, add string
+        if not removeCharList[ord(curChar)]:
+            outputString[insertPoint] = outputString[i]
+            insertPoint += 1
+    
+    #ideally, null terminate this at the end at position insertPoint
+    #python: not sure how to do this, except for return the string up to that point
+    outputString = str(outputString[:insertPoint].decode("utf-8"))
+    
+    print(outputString)
